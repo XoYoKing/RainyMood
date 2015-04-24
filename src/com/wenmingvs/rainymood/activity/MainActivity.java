3ded992f0da9e79e3bdfd650d3483f410ae12b22
wenmingvs <wenmingvs@gmail.com>
2015-04-24 19:28:06 +0800

    决定还是把广告代码留在主分支方便后期调试，这次提交更新的功能有1.防止被防编译 2.添加给我建议功能 3.加入迷你广告和积分墙，并且在云端决定是否开启客户端的广告功能

--- src/com/wenmingvs/rainymood/activity/MainActivity.java
+++ src/com/wenmingvs/rainymood/activity/MainActivity.java
@@ -1,6 +1,7 @@
 package com.wenmingvs.rainymood.activity;
 
 
+
 import android.app.Activity;
 import android.content.Context;
 import android.content.Intent;
@@ -12,10 +13,13 @@ import android.util.Log;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.view.Window;
+import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
 import android.widget.SeekBar;
 import android.widget.SeekBar.OnSeekBarChangeListener;
 import android.widget.TextView;
+import cn.waps.AppConnect;
 
 import com.wenmingvs.rainymood.R;
 import com.wenmingvs.rainymood.service.CounterService;
@@ -34,14 +38,23 @@ public class MainActivity extends Activity implements OnClickListener{
 	private SeekBar thunderBar;
 	private AudioManager audioManager;
 	private int maxVolume;
-	private SlidingMenu mLeftMenu ; 
+
 	private TextView weibo;
 	private TextView zhihu;
-	private TextView github;
+	
+	private Button adviceButton;
+	private String ShowAd = "";
+	private SlidingMenu mLeftMenu ; 
+	
 	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
+		
+		AppConnect.getInstance("033b920ab379609d1297fda7bcfa8c41","default",this);
+		
+		ShowAd = AppConnect.getInstance(this).getConfig("ShowAd","false");
+		
 		ActivityCollector.addActivity(this);
 		requestWindowFeature(Window.FEATURE_NO_TITLE);
 		setContentView(R.layout.activity_main);
@@ -52,6 +65,8 @@ public class MainActivity extends Activity implements OnClickListener{
 	
 	private void init_Buttonand_other()
 	{
+		
+		
 		playorstopButton = (ImageView)findViewById(R.id.playorstop);
 		counter = (ImageView)findViewById(R.id.counter_imageview);
 		about = (ImageView)findViewById(R.id.about);
@@ -59,14 +74,25 @@ public class MainActivity extends Activity implements OnClickListener{
 		rainBar = (SeekBar)findViewById(R.id.rain_seekbar);
 		thunderBar = (SeekBar)findViewById(R.id.thunder_seekbar);
 		mLeftMenu = (SlidingMenu) findViewById(R.id.id_menu);
-
 		weibo = (TextView)findViewById(R.id.weibo);
 		zhihu = (TextView)findViewById(R.id.zhihu);
-		github = (TextView)findViewById(R.id.github);
+		
+		
+		adviceButton = (Button)findViewById(R.id.advice);
+		
+		//显示迷你广告
+		
+		if (ShowAd.equals("true")) {
+			LinearLayout miniLayout = (LinearLayout) findViewById(R.id.miniAdLinearLayout);
+			AppConnect.getInstance(this).showMiniAd(this, miniLayout, 10);// 10秒刷新一次
+		}
+		
+		
+		
 		
 		weibo.setMovementMethod(LinkMovementMethod.getInstance());
 		zhihu.setMovementMethod(LinkMovementMethod.getInstance());
-		github.setMovementMethod(LinkMovementMethod.getInstance());
+		
 		
 		
 		audioManager = (AudioManager)getSystemService(Context.AUDIO_SERVICE);
@@ -122,11 +148,20 @@ public class MainActivity extends Activity implements OnClickListener{
 			}
 		break;
 		case R.id.about:
-			vibrate_1s();
-			mLeftMenu.toggle();
+			
+			if (ShowAd.equals("true")) {
+				vibrate_1s();
+				AppConnect.getInstance(this).showOffers(this);
+			}else {
+				vibrate_1s();
+				mLeftMenu.toggle();
+			}
 		break;
-		default:
-			break;
+
+		case R.id.advice:
+			vibrate_1s();
+			AppConnect.getInstance(this).showFeedback(this);
+		
 		}
 	}
 	
@@ -135,7 +170,7 @@ public class MainActivity extends Activity implements OnClickListener{
 		playorstopButton.setOnClickListener(this);
 		counter.setOnClickListener(this);
 		about.setOnClickListener(this);
-		
+		adviceButton.setOnClickListener(this);
 			
 		
 		
@@ -204,6 +239,7 @@ public class MainActivity extends Activity implements OnClickListener{
 		Intent intent2 = new Intent(this, CounterService.class);
 		stopService(intent);
 		stopService(intent2);
+		AppConnect.getInstance(this).close();
 		super.onDestroy();
 	}
 	private void playMusic()
